eb279756c72e528c5f6b36defa6e17acba4393de
Nguyễn Minh Phú <phu.197pm33529@gmail.com>
2021-11-21 12:06:27 +0700

    hoàn tất chức năng login bên customer

--- app/src/main/java/com/example/androidproject_coupon/User/MainActivity_User.java
+++ app/src/main/java/com/example/androidproject_coupon/User/MainActivity_User.java
@@ -5,17 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.androidproject_coupon.AccountManagement.GetIDandRole;
 import com.example.androidproject_coupon.AccountManagement.Login;
+import com.example.androidproject_coupon.AccountManagement.RegisterAccount;
 import com.example.androidproject_coupon.MainActivity;
 import com.example.androidproject_coupon.ProductFragment;
 import com.example.androidproject_coupon.R;
 
 public class MainActivity_User extends AppCompatActivity {
+
+    Integer check, role;
+    GetIDandRole idAndRole = new GetIDandRole();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +31,34 @@ public class MainActivity_User extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.main_menu,menu);
+        check = idAndRole.id;
+        role = idAndRole.role;
+        Log.d("Kiem tra:", String.valueOf(role));
+        if (check == 0){
+            menuInflater.inflate(R.menu.main_menu,menu);
+        }else{
+            if (role == 1){
+                menuInflater.inflate(R.menu.menu_logout_admin_inuser,menu);
+            }else{
+                menuInflater.inflate(R.menu.menu_logout,menu);
+            }
+        }
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(item.getItemId() == R.id.mnuAdmin){
-            finish();
+            startActivity(new Intent(MainActivity_User.this, MainActivity.class));
         }
-        else if(item.getItemId() == R.id.mnuLogout){
-            Toast.makeText(getApplicationContext(), "Chao mung ban den voi mon \n Chuyen de lap trinh di dong", Toast.LENGTH_LONG).show();
+        else if(item.getItemId() == R.id.mnuLogout || item.getItemId() == R.id.mnuLogout_admin_inuser){
+            Toast.makeText(getApplicationContext(), "Bạn đã đăng xuất", Toast.LENGTH_LONG).show();
+            idAndRole.id = 0;
+            startActivity(new Intent(MainActivity_User.this,MainActivity.class));
         }
         else if(item.getItemId()==R.id.mnuLogin){
-            startActivity(new Intent(MainActivity_User.this, Login.class));
-        }
-        else if(item.getItemId()==R.id.mnuUser){
-            startActivity(new Intent(getApplicationContext(), MainActivity_User.class));
+            startActivity(new Intent(MainActivity_User.this,Login.class));
+            //check = 1;
         }
         return super.onOptionsItemSelected(item);
     }
