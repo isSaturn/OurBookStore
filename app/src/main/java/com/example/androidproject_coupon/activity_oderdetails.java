edd6dbb3a4660cd8f011173ddcc007fdedaf0ef8
phanthihuyendieu <phanthihuyendieuuu@gmail.com>
2021-11-22 11:28:02 +0700

    fix click on

--- app/src/main/java/com/example/androidproject_coupon/activity_oderdetails.java
+++ app/src/main/java/com/example/androidproject_coupon/activity_oderdetails.java
@@ -3,12 +3,25 @@ package com.example.androidproject_coupon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class activity_oderdetails extends AppCompatActivity {
 
+    ImageView imback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oderdetails);
+
+        imback = (ImageView) findViewById(R.id.im_details_back);
+
+        imback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
