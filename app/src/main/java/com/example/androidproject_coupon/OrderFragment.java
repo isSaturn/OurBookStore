8ec8db8e5fbcefd92d2f744ac5d01f17b391f96d
phanthihuyendieu <phanthihuyendieuuu@gmail.com>
2021-11-24 13:19:27 +0700

    commit

--- app/src/main/java/com/example/androidproject_coupon/OrderFragment.java
+++ app/src/main/java/com/example/androidproject_coupon/OrderFragment.java
@@ -8,14 +8,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.example.androidproject_coupon.CouponManagement.Coupon.Coupon;
+import com.example.androidproject_coupon.CouponManagement.CouponAdapter;
 import com.example.androidproject_coupon.OrderManagement.Oder;
 import com.example.androidproject_coupon.OrderManagement.Oder_adapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -82,17 +90,56 @@ public class OrderFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        listView = view.findViewById(R.id.lv_status_oder);
-        dataList.add(new Oder("04/11/2021 14:00", "211104SACH0001", "Cho xac nhan", "85.000d", "69/68 Dang Thuy Tram"));
-        oder_adapter = new Oder_adapter(getContext(),R.layout.item_oder, dataList);
-        listView.setAdapter(oder_adapter);
+      listView = view.findViewById(R.id.lv_status_oder);
+//        dataList.add(new Oder("04/11/2021 14:00", "211104SACH0001", "Cho xac nhan", "85.000d", "69/68 Dang Thuy Tram"));
+//        oder_adapter = new Oder_adapter(getContext(),R.layout.item_oder, dataList);
+//        listView.setAdapter(oder_adapter);
 
+
+
+        ListView listView = view.findViewById(R.id.list_view);
+        String TAG="FIREBASE";
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("DonHang");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot data: snapshot.getChildren()){
+                    String time = data.child("Time").getValue().toString();
+                    String code = data.child("Ma_Don_Hang").getValue().toString();
+                    String status = data.child("ID_Trang_Thai_DH").getValue().toString();
+                    String price = data.child("Tong_Tien").getValue().toString();
+                    String address = data.child("Dia_Chi").getValue().toString();
+                    String name = data.child("Ho_Ten").getValue().toString();
+                    String phone = data.child("SDT").getValue().toString();
+                    String hinhthuc = data.child("ID_Hinh_Thuc_GH").getValue().toString();
+                    String khuyenmai = data.child("ID_Khuyen_Mai").getValue().toString();
+                    String taikhoan = data.child("ID_Tai_Khoan").getValue().toString();
+
+                    dataList.add(new Oder(address,time, code, status, price));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        Oder_adapter oder_adapter  = new Oder_adapter(getContext(), R.layout.item_oder, dataList);
+        listView.setAdapter(oder_adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
-                Intent intent = new Intent(getContext(), activity_oderdetails.class);
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent ( getContext(), EditCoupon.class);
+                intent.putExtra( "code", dataList.get(i).getCode());
+                intent.putExtra( "time", dataList.get(i).getTime());
+                intent.putExtra( "status", dataList.get(i).getStatus());
+                intent.putExtra( "price", dataList.get(i).getPrice().toString());
+                intent.putExtra( "address", dataList.get(i).getAddress());
                 startActivity(intent);
             }
         });
+        }
     }
-}
--- app/src/main/java/com/example/androidproject_coupon/activity_oderdetails.java
+++ app/src/main/java/com/example/androidproject_coupon/activity_oderdetails.java
@@ -1,14 +1,42 @@
 package com.example.androidproject_coupon;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class activity_oderdetails extends AppCompatActivity {
+
+    TextView tvgiaohang, tvname, tvaddress, tvphone, tvcode, tvtime, tvstatus;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oderdetails);
+
+        matching();
+        Intent intent = getIntent();
+        Integer tvgiaohang = intent.getIntExtra("hinhthuc",1);
+        String tvname = intent.getStringExtra("name");
+        String tvaddress= intent.getStringExtra( "address");
+        String tvphone = intent.getStringExtra( "phone");
+        String tvcode = intent.getStringExtra( "code");
+        String tvtime = intent.getStringExtra( "time");
+        String tvstatus = intent.getStringExtra( "status, ");
+    }
+
+    private void matching() {
+        tvgiaohang = findViewById(R.id.tv_details_giaohang);
+        tvname = findViewById(R.id.tv_details_name);
+        tvaddress = findViewById(R.id.tv_details_address);
+        tvphone = findViewById(R.id.tv_details_phone);
+        tvcode = findViewById(R.id.tv_details_madonhang);
+        tvtime = findViewById(R.id.tv_details_timeorder);
+        tvstatus = findViewById(R.id.tv_details_choxn);
     }
 }
