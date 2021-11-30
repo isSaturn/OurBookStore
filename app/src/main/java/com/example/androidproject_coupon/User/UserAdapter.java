281dcb5c10ee5828cdf1f19ffb26d485a77b16be
197PM31219 - TÔ MINH TRIẾT - K25T-PM1 <triet.197pm31219@vanlanguni.vn>
2021-11-30 22:29:58 +0700

    Sửa layout trang chủ

--- app/src/main/java/com/example/androidproject_coupon/User/UserAdapter.java
+++ app/src/main/java/com/example/androidproject_coupon/User/UserAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -70,7 +71,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
 
     public class UserViewHolder extends RecyclerView.ViewHolder {
 
-        private ConstraintLayout layout;
+        private RelativeLayout layout;
         public TextView textViewTenSach, textViewGiaTien;
         public ImageView imageView;
 
